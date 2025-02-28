dd4ef602826d9b8378fd0f8c3a1b94758b7e7fcd
Your Name <abodjameel@gmail.com>
2025-03-01 00:00:21 +0300

    1

---
+++ odoo/custom_addons/app_one/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Reminser',
+    'author': 'abdulla jameel',
+    'version': '1.0',
+    'depends':['Base',]
+    'data':[],
+    'application':True,
+
+}
